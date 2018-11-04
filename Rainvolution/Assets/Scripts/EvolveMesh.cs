db8b7ac6a5c13d0df040113191659e61c2b602dc
Laura Calle <laucalle09@gmail.com>
2018-11-04 01:24:46 +0100

    evolution system and assets

---
+++ Rainvolution/Assets/Scripts/EvolveMesh.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolveMesh : MonoBehaviour
+{
+    public ParticleSystem puff;
+    public GameObject[] planets;
+    GameManager game;
+    int active_planet = 0;
+    // Use this for initialization
+    void Start()
+    {
+        game = GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (game.vegetals >= 20 && game.vegetals < 50)
+        {
+            if (active_planet != 1)
+            {
+                planets[active_planet].SetActive(false);
+                puff.Play();
+                planets[1].SetActive(true);
+                active_planet = 1;
+
+            }
+
+        }
+        else if (game.vegetals >= 50 && game.vegetals < 150)
+        {
+            if (active_planet != 2)
+            {
+                planets[active_planet].SetActive(false);
+                puff.Play();
+                planets[2].SetActive(true);
+                active_planet = 2;
+            }
+        }
+        else if (game.vegetals >= 150)
+        {
+            if (active_planet != 3)
+            {
+                planets[active_planet].SetActive(false);
+                puff.Play();
+                planets[3].SetActive(true);
+                active_planet = 3;
+
+            }
+        }
+        else if (game.vegetals < 20) {
+            if (active_planet != 0)
+            {
+                planets[active_planet].SetActive(false);
+                puff.Play();
+                planets[0].SetActive(true);
+                active_planet = 0;
+
+            }
+        }
+    }
+}
