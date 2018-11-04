aae114a5f3add3932f44533545a3480ac4766314
fdoblas79 <tulusintulus@gmail.com>
2018-11-04 19:14:52 +0100

    script Bar
    Incluye las 3 barras y el script que controla el progreso de la barra conviertiendo de 0 a 1 los valores máximos y mínimos independientemente del rango de valores de cada barra.

---
+++ Rainvolution/Assets/Scripts/Bar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour {
+
+
+   
+    public float fillAmount;
+    [SerializeField]
+    private float Maximo;
+    [SerializeField]
+    private float Minimo;
+    [SerializeField]
+    private Image content;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+    content.fillAmount = Map(fillAmount,Minimo,Maximo,0,1); 
+    }
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
